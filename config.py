b31fe878316251642603c6c83caa9c280a192ab1
Smatti2 <mattiasolarino@hotmail.com>
2025-01-13 17:32:26 +0100

    create config class

---
+++ config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings
+
+
+class AppConfig(BaseSettings):
+    COIN_API_KEY: str
+
+    class Config:
+        # Loads the environment variables from a '.env' file
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+config = AppConfig()
+# Example of using the configuration
+if __name__ == "__main__":
+    config = AppConfig()
+    print(config.COIN_API_KEY)
