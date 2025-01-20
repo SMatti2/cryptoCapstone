8936f0e75f9cc31eacd8a351a8d7d5fcf8f6b46a
Smatti2 <mattiasolarino@hotmail.com>
2025-01-20 10:22:49 +0100

    add create tables to db file

--- src/db.py
+++ src/db.py
@@ -1,4 +1,11 @@
-from sqlmodel import create_engine
+from sqlmodel import create_engine, SQLModel
 from config import config
 
+from src.models.post import Post
+from src.models.comment import Comment
+
 engine = create_engine(config.DATABASE_URL, echo=False)
+
+# create all tables defined in SQLModel classes
+SQLModel.metadata.create_all(engine)
+print("Tables created successfully!")
