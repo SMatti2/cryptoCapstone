d1d07a9cd3e9b9ca6bed27cb20d3265ff716aa06
Smatti2 <mattiasolarino@hotmail.com>
2025-01-20 09:29:44 +0100

    create comment model

---
+++ src/models/comment.py
@@ -0,0 +1,17 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+
+
+class Comment(SQLModel, table=True):
+    id: str = Field(default=None, primary_key=True)
+    subreddit: Optional[str] = Field(default=None, nullable=True)
+    author: Optional[str] = Field(default=None, nullable=True)
+    body: Optional[str] = Field(default=None, nullable=True)
+    created_utc: Optional[int] = Field(default=None, nullable=True)
+    link_id: Optional[str] = Field(default=None, nullable=True)
+    controversiality: Optional[int] = Field(default=None, nullable=True)
+    ups: Optional[int] = Field(default=None, nullable=True)
+    score: Optional[int] = Field(default=None, nullable=True)
+    gilded: Optional[int] = Field(default=None, nullable=True)
+    retrieved_on: Optional[int] = Field(default=None, nullable=True)
+    distinguished: Optional[str] = Field(default=None, nullable=True)
