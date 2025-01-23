9199b87b17f59d9e49744f4ceba0dcf05106a6bc
Smatti2 <mattiasolarino@hotmail.com>
2025-01-23 11:51:04 +0100

    add pp test to check for stationarity

--- src/preprocessing/transformations/stationarity.py
+++ src/preprocessing/transformations/stationarity.py
@@ -1,6 +1,8 @@
 import numpy as np
 from statsmodels.tsa.stattools import adfuller
 
+from arch.unitroot import PhillipsPerron
+
 
 def adf_test(series, alpha):
     """Augmented Dickey-Fuller test"""
@@ -8,6 +10,17 @@ def adf_test(series, alpha):
     p_value = adf_result[1]
     return {
         "Test Statistic": adf_result[0],
+        "p-value": p_value,
+        "Stationary": p_value < alpha,
+    }
+
+
+def pp_test(series, alpha):
+    """Perform Phillips-Perron test."""
+    pp_result = PhillipsPerron(series)
+    p_value = pp_result.pvalue
+    return {
+        "Test Statistic": pp_result.stat,
         "p-value": p_value,
         "Stationary": p_value < alpha,
     }
