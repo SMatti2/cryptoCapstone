9ef2646a2a5e707079f71cd401ca0cbfb904669b
Smatti2 <mattiasolarino@hotmail.com>
2025-01-23 14:20:15 +0100

    create the fuction to use the kpss test

--- src/preprocessing/transformations/stationarity.py
+++ src/preprocessing/transformations/stationarity.py
@@ -1,6 +1,9 @@
-import numpy as np
-from statsmodels.tsa.stattools import adfuller
+import pandas as pd
 
+import warnings
+import numpy as np
+from statsmodels.tsa.stattools import adfuller, kpss
+from statsmodels.tools.sm_exceptions import InterpolationWarning
 from arch.unitroot import PhillipsPerron
 
 
@@ -16,11 +19,22 @@ def adf_test(series, alpha):
 
 
 def pp_test(series, alpha):
-    """Perform Phillips-Perron test."""
+    """Phillips-Perron test"""
     pp_result = PhillipsPerron(series)
     p_value = pp_result.pvalue
     return {
         "Test Statistic": pp_result.stat,
         "p-value": p_value,
         "Stationary": p_value < alpha,
+    }
+
+
+def kpss_test(series, alpha):
+    """KPSS test"""
+    kpss_stat = kpss(series, regression="c", nlags="auto")
+    p_value = kpss_stat[1]
+    return {
+        "Test Statistic": kpss_stat,
+        "p-value": p_value,
+        "Stationary": p_value >= alpha,
     }
