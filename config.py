6428f779606a6954192bcac79244769a08c48c83
Smatti2 <mattiasolarino@hotmail.com>
2025-02-03 12:40:21 +0100

    clean useless comments

--- config.py
+++ config.py
@@ -42,13 +42,12 @@ class AppConfig(BaseSettings):
     ]
 
     class Config:
-        # Loads the environment variables from a '.env' file
         env_file = ".env"
         env_file_encoding = "utf-8"
 
 
 config = AppConfig()
-# Example of using the configuration
+
 if __name__ == "__main__":
     config = AppConfig()
     print(config.COIN_API_KEY)
