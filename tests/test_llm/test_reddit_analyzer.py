3db49f86a283f6b08ae2478366eab3c48bd3aa9d
Smatti2 <mattiasolarino@hotmail.com>
2025-02-13 10:15:40 +0100

    simplify tests and insert them in a class

--- tests/test_llm/test_reddit_analyzer.py
+++ tests/test_llm/test_reddit_analyzer.py
@@ -1,114 +1,113 @@
-import pytest
-from sqlmodel import Session, SQLModel, create_engine
-from src.llm.reddit_analyzer import (
-    get_total_posts,
-    get_total_comments,
-    get_earliest_posts,
-    get_earliest_comments,
-    print_post_samples,
-    print_comment_samples,
-    show_post_details,
-    show_comment_details,
-)
+import unittest
+from datetime import datetime
+from sqlmodel import SQLModel
+
+from src.llm.reddit_analyzer import RedditAnalyzer
 from src.models.post import Post
 from src.models.comment import Comment
 
-
-@pytest.fixture(scope="module")
-def session():
-    engine = create_engine("sqlite:///:memory:")
-    SQLModel.metadata.create_all(engine)
-    with Session(engine) as session:
-        yield session
+TEST_DATABASE_URL = "sqlite:///:memory:"
 
 
-@pytest.fixture(scope="module")
-def sample_data(session):
-    posts = [
-        Post(
+class TestRedditAnalyzer(unittest.TestCase):
+    def setUp(self):
+        self.analyzer = RedditAnalyzer(TEST_DATABASE_URL)
+        SQLModel.metadata.create_all(self.analyzer.engine)
+
+        post1 = Post(
             id="1",
-            subreddit="test",
-            title="Test Post 1",
-            created_utc=1000,
-            author="test_author1",
-            num_comments=0,
-            score=1,
+            subreddit="subreddit1",
+            title="Post 1",
+            selftext="Content 1",
+            author="author1",
+            created_utc=int(datetime.now().timestamp()),
+            num_comments=10,
+            score=100,
+            url="http://example.com/post1",
             archived=False,
+            domain="example.com",
+            permalink="/r/subreddit1/post1",
             is_video=False,
-            url="https://example.com/post1",
-            permalink="/r/test/comments/1/test_post_1",
-        ),
-        Post(
+        )
+        post2 = Post(
             id="2",
-            subreddit="test",
-            title="Test Post 2",
-            created_utc=2000,
-            author="test_author2",
-            num_comments=0,
-            score=1,
-            archived=False,
-            is_video=False,
-            url="https://example.com/post2",
-            permalink="/r/test/comments/2/test_post_2",
-        ),
-    ]
-    comments = [
-        Comment(id="1", subreddit="test", body="Test Comment 1", created_utc=1500),
-        Comment(id="2", subreddit="test", body="Test Comment 2", created_utc=2500),
-    ]
-    session.add_all(posts + comments)
-    session.commit()
+            subreddit="subreddit2",
+            title="Post 2",
+            selftext="Content 2",
+            author="author2",
+            created_utc=int(datetime.now().timestamp()),
+            num_comments=20,
+            score=200,
+            url="http://example.com/post2",
+            archived=True,
+            domain="example.com",
+            permalink="/r/subreddit2/post2",
+            is_video=True,
+        )
+        comment1 = Comment(
+            id="1",
+            subreddit="subreddit1",
+            author="author1",
+            body="Comment 1",
+            created_utc=int(datetime.now().timestamp()),
+            link_id="1",
+            controversiality=0,
+            ups=10,
+            score=10,
+            gilded=0,
+            retrieved_on=int(datetime.now().timestamp()),
+            distinguished=None,
+            post_id="1",
+        )
+        comment2 = Comment(
+            id="2",
+            subreddit="subreddit2",
+            author="author2",
+            body="Comment 2",
+            created_utc=int(datetime.now().timestamp()),
+            link_id="2",
+            controversiality=1,
+            ups=20,
+            score=20,
+            gilded=1,
+            retrieved_on=int(datetime.now().timestamp()),
+            distinguished="moderator",
+            post_id="2",
+        )
 
+        self.analyzer.session.add_all([post1, post2, comment1, comment2])
+        self.analyzer.session.commit()
 
-def test_get_total_posts(session, sample_data):
-    assert get_total_posts(session) == 2
+    def tearDown(self):
+        self.analyzer.close()
 
+    def test_get_total_posts(self):
+        total_posts = self.analyzer.get_total_posts()
+        self.assertEqual(total_posts, 2)
 
-def test_get_total_comments(session, sample_data):
-    assert get_total_comments(session) == 2
+    def test_get_total_comments(self):
+        total_comments = self.analyzer.get_total_comments()
+        self.assertEqual(total_comments, 2)
 
+    def test_context_manager(self):
+        with RedditAnalyzer(TEST_DATABASE_URL) as analyzer:
+            SQLModel.metadata.create_all(analyzer.engine)
 
-def test_get_earliest_posts(session, sample_data):
-    earliest_posts = get_earliest_posts(session)
-    assert len(earliest_posts) == 2
-    assert earliest_posts[0].created_utc == 1000
+            post = Post(
+                id="99",
+                subreddit="some_sub",
+                title="Another Post",
+                author="someone",
+                created_utc=int(datetime.now().timestamp()),
+                num_comments=0,
+                score=0,
+                url="http://example.com/another",
+                archived=False,
+                domain="example.com",
+                permalink="/r/some_sub/another",
+                is_video=False,
+            )
+            analyzer.session.add(post)
+            analyzer.session.commit()
 
-
-def test_get_earliest_comments(session, sample_data):
-    earliest_comments = get_earliest_comments(session)
-    assert len(earliest_comments) == 2
-    assert earliest_comments[0].created_utc == 1500
-
-
-def test_print_post_samples(capsys, sample_data):
-    posts = [Post(id="1", subreddit="test", title="Test Post", created_utc=1000)]
-    print_post_samples(posts)
-    captured = capsys.readouterr()
-    assert "1. ID: 1, Subreddit: test" in captured.out
-
-
-def test_print_comment_samples(capsys, sample_data):
-    comments = [
-        Comment(id="1", subreddit="test", body="Test Comment", created_utc=1000)
-    ]
-    print_comment_samples(comments)
-    captured = capsys.readouterr()
-    assert "1. ID: 1, Subreddit: test" in captured.out
-
-
-def test_show_post_details(capsys, sample_data):
-    posts = [Post(id="1", subreddit="test", title="Test Post", created_utc=1000)]
-    show_post_details(posts)
-    captured = capsys.readouterr()
-    assert "Post ID: 1" in captured.out
-    assert "Subreddit: test" in captured.out
-
-
-def test_show_comment_details(capsys, sample_data):
-    comments = [
-        Comment(id="1", subreddit="test", body="Test Comment", created_utc=1000)
-    ]
-    show_comment_details(comments)
-    captured = capsys.readouterr()
-    assert "Comment ID: 1" in captured.out
-    assert "Subreddit: test" in captured.out
+            self.assertEqual(analyzer.get_total_posts(), 1)
